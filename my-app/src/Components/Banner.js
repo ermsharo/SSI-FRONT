a781bf8c89d48c0e3ebedeeb6c5a17e187ed9b7a
Ricardo Augusto Fernandes Junior <rafernandesjr@usp.br>
2020-06-29 21:07:05 -0300

    Merge branch 'master' of https://github.com/ermsharo/SSI-FRONT

--- my-app/src/Components/Banner.js
+++ my-app/src/Components/Banner.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import styled, { css } from 'styled-components'
 
-const BannerBox = styled.div`
- 
-width: 100vw;
-background-color:white;
-height:auto; 
-margin-top:0px;
+function Banner() {
 
-
-
+const GenericSize= styled.div`
+     width:100vw; 
+    height:100vh; 
+    color:white;
+    background-color:lightgoldenrodyellow; 
+    line-height:50vw; 
+    color:black; 
+    text-align:center; 
+    font-size:60px; 
+    font-family:var(--fontTitulo);
 `;
 
-function Banner() {
   return (
-    
-    <BannerBox><img  style ={{width:'100%',height:'auto'}} src = "https://static.vecteezy.com/system/resources/previews/001/058/378/non_2x/purple-gradient-overlapping-shape-horizontal-banner-vector.jpg"></img></BannerBox>
+    <GenericSize>
+    Banner
+    </GenericSize>
   );
 }
 
--- my-app/src/Components/Programacao.js
+++ my-app/src/Components/Programacao.js
@@ -1,11 +1,23 @@
 import React from 'react';
-
-
+import styled, { css } from 'styled-components'
 
 function Programacao() {
+
+  const GenericSize= styled.div`
+     width:100vw; 
+    height:100vh; 
+    color:white;
+    background-color:lightslategrey; 
+    line-height:50vw; 
+    color:black; 
+    text-align:center; 
+    font-size:60px; 
+    font-family:var(--fontTitulo);
+`;
+
   return (
     
-    <div>Header</div>
+    <GenericSize>Programação</GenericSize>
   );
 }
 
